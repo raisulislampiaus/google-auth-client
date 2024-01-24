c0ec87dcb8da383d073207996744b904fd57837f
raisulislampiaus <raisulislam714@gmail.com>
2024-01-24 23:28:04 +0600

    auth client

---
+++ src/Components/Headers.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import "./header.css"
+import { NavLink } from "react-router-dom"
+import axios from "axios"
+
+const Headers = () => {
+    const [userdata, setUserdata] = useState({});
+    console.log("response", userdata)
+
+    const getUser = async () => {
+        try {
+            const response = await axios.get("https://google-auth-mern.vercel.app/login/sucess", { withCredentials: true });
+
+            setUserdata(response.data.user)
+           
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+    // logoout
+    const logout = ()=>{
+        window.open("https://google-auth-mern.vercel.app/logout","_self")
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+    return (
+        <>
+            <header>
+                <nav>
+                    <div className="left">
+                        <h1>HEY</h1>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            
+                            {
+                                Object?.keys(userdata)?.length > 0 ? (
+                                    <>
+                                    <li style={{color:"black",fontWeight:"bold"}}>{userdata?.displayName}</li>
+                                        
+                                        <li onClick={logout}>Logout</li>
+                                        <li>
+                                            <img src={userdata?.image} style={{ width: "50px", borderRadius: "50%" }} alt="no-images" />
+                                        </li>
+                                    </>
+                                ) : <li>
+                                    <NavLink to="/">
+                                        Login
+                                    </NavLink>
+                                </li>
+                            }
+
+
+
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Headers
