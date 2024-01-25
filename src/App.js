0ae9143be3eebb4d21419e9abdb7f55ca1d446c2
Raisul islam piaus <54791396+raisulislampiaus@users.noreply.github.com>
2024-01-25 14:33:06 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Components/Home';
 import Headers from './Components/Headers';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
-import Error from './Components/Error';
+
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='*' element={<Error />} />
+        
       </Routes>
     </>
   );
